8263bd615520d4538db9bd9a27d7b9aaa445faa0
joaootavio93 <joao_otavio93@live.com>
2017-05-03 19:55:59 -0300

    Adicionando normalização

--- knn.py
+++ knn.py
@@ -1,19 +1,23 @@
-from Util import readBase
 import math
 import operator
+from Util import readBase
 
-def getEuclideanDistance(instance1, instance2, length):
+def normalize(raw_set):
+    normalized = raw_set - raw_set.mean() / raw_set.std()
+    return normalized
+
+def getEuclideanDistance(instance_1, instance_2, length):
 	distance = 0
 	for i in range(length):
-		distance += pow((instance1[i] - instance2[i]), 2)
+		distance += pow((instance_1[i] - instance_2[i]), 2)
 	return math.sqrt(distance)
 
-def getNeighbors(testInstance, trainingSet, trainingLabels, k):
+def getNeighbors(test_instance, train_set, train_labels, k):
     distances = []
-    viewLength = len(testInstance) - 1
-    for i in range(len(trainingSet)):
-        distance = getEuclideanDistance(testInstance, trainingSet[i], viewLength)
-        distances.append((distance, trainingLabels[i]))
+    view_length = len(test_instance) - 1
+    for i in range(len(train_set)):
+        distance = getEuclideanDistance(test_instance, train_set[i], view_length)
+        distances.append((distance, train_labels[i]))
     distances.sort(key=operator.itemgetter(0))
     neighbors = []
     for i in range(k):
@@ -21,41 +25,43 @@ def getNeighbors(testInstance, trainingSet, trainingLabels, k):
     return neighbors
 
 def getVotes(neighbors):
-    labelsVotes = {}
+    votes = {}
     for i in range(len(neighbors)):
-        result = neighbors[i]
-        if result in labelsVotes:
-            labelsVotes[result] += 1
+        if neighbors[i] in votes:
+            votes[neighbors[i]] += 1
         else:
-            labelsVotes[result] = 1
-    votes = sorted(labelsVotes.items(), key=operator.itemgetter(0), reverse=True)
+            votes[neighbors[i]] = 1
+    votes = sorted(votes.items(), key=operator.itemgetter(0), reverse=True)
     return votes[0][0]
 
-def getAccuracy(testLabels, predictions):
+def getAccuracy(test_labels, predictions):
     matches = 0
-    for i in range(len(testLabels)):
-        if testLabels[i] == predictions[i]:
+    for i in range(len(test_labels)):
+        if test_labels[i] == predictions[i]:
             matches += 1
-    return (matches / float(len(testLabels))) * 100.0
+    return (matches / float(len(test_labels))) * 100.0
 
-def knn(testSet, testLabels, trainingSet, trainingLabels, k):
+def knn(test_set, test_labels, train_set, train_labels, k):
     predictions = []
-    for i in range(len(testSet)):
-        print("instance " + repr(i) + ":")
-        neighbors = getNeighbors(testSet[i], trainingSet, trainingLabels, k)
-        print("neighbors = " + repr(neighbors))
+    # train_set = normalize(train_set)
+    # test_set = normalize(test_set)
+    for i in range(len(test_set)):
+        neighbors = getNeighbors(test_set[i], train_set, train_labels, k)
         result = getVotes(neighbors)
         predictions.append(result)
-        print('predicted = ' + repr(result) + ', actual = ' + repr(testLabels[i]))
-        print("====== end =====")
-    accuracy = getAccuracy(testLabels, predictions)
-    print("Accuracy: " + repr(accuracy) + "%")
+        # print("instance " + repr(i) + ":")
+        # print("neighbors = " + repr(neighbors))
+        # print('predicted = ' + repr(result) + ', actual = ' + repr(test_labels[i]))
+        # print("====== end =====")
+    accuracy = getAccuracy(test_labels, predictions)
+    print("accuracy: " + repr(accuracy) + "%")
     return predictions
 
 def main():
-    shapeTraining, rgbTraining, labelsTraining = readBase('segmentation.data')
-    shapeTest, rgbTest, labelsTest = readBase('segmentation.test')
+    shape_train, rgb_train, labels_train = readBase('segmentation.data')
+    shape_test, rgb_test, labels_test = readBase('segmentation.test')
     k = 7
-    # knn(shapeTest, labelsTest, shapeTraining, labelsTraining, k)
-    knn(rgbTest, labelsTest, rgbTraining, labelsTraining, k)
+    knn(shape_test, labels_test, shape_train, labels_train, k)
+    knn(rgb_test, labels_test, rgb_train, labels_train, k)
+
 main()
